f427423894b37f3e602ba0fc4e93f6ff7bb56729
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-25 15:33:13 +0100

    add code to connect to db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hiya!");
+const mysql = require("mysql");
+
+const dbOptions = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employee_tracker_db",
+};
+
+const connection = mysql.createConnection(dbOptions);
+
+const onConnect = (err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  connection.end();
+};
+
+connection.connect(onConnect);
