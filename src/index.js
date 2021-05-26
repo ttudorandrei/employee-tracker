8e5590cf627be66c81ccad00c60d33d24144a6e2
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-26 21:04:10 +0100

    move start function from index.js to the DB.js file, and just calling it in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // dependencies
-const Db = require("./db/index");
+const Db = require("./db/DB");
 const db = new Db("employee_tracker_db");
 
 // If the connection is unsuccessful, the code below will throw an error, else will execute whatever code we choose to put there;
