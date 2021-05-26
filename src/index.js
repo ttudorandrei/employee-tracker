a8ba998107e63a5d0eb0d3d2c7885ea72f433251
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-26 14:33:41 +0100

    add comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// dependencies
 const mysql = require("mysql");
 
+// object containing our connection setup
 const dbOptions = {
   host: "localhost",
   port: 3306,
@@ -8,12 +10,16 @@ const dbOptions = {
   database: "employee_tracker_db",
 };
 
+// we create a connection using the object containing all of our connection data (port, host, password, database and user)
 const connection = mysql.createConnection(dbOptions);
 
+// If the connection is unsuccessful, the code below will throw an error, else will execute whatever code we choose to put there;
 const onConnect = (err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
+  // this will end
   connection.end();
 };
 
+// this will try and connect accepting a callback
 connection.connect(onConnect);
